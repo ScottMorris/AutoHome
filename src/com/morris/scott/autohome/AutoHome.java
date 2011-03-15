ed0617bdfff9aa44473e4e60c3a2ec2586d02a64
Scott Morris <scott.moris@gmail.com>
2011-03-14 22:04:22 -0400

    Added to GIT

---
+++ src/com/morris/scott/autohome/AutoHome.java
@@ -0,0 +1,72 @@
+package com.morris.scott.autohome;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class AutoHome extends Activity {
+    /** Called when the activity is first created.
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+    }*/
+	GridView lobbyGridView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		lobbyGridView = (GridView)findViewById(R.id.CommonTasks);
+		lobbyGridView.setAdapter(new iconAdapter(this));
+	}
+	
+	public class iconAdapter extends BaseAdapter {
+		private Context mContext;
+		private int[] mIcons = {R.drawable.pvr_unpressed, R.drawable.climate_unpressed};
+		
+		public iconAdapter(Context c) {
+			mContext = c;
+		}
+
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return mIcons.length;
+		}
+
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ImageView imageView;
+	        if (convertView == null) {  // if it's not recycled, initialize some attributes
+				imageView = new ImageView(mContext);
+				imageView.setLayoutParams(new GridView.LayoutParams(96, 96));
+				imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+				imageView.setPadding(8, 8, 8, 8);
+	        } else {
+	            imageView = (ImageView) convertView;
+	        }
+
+	        imageView.setImageResource(mIcons[position]);
+
+			return imageView;
+		}
+		
+
+	}
+}
