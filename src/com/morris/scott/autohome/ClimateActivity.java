d6a216168718d1ceb4a7c2f835d4e2bcb3a4337d
Scott Morris <scott.moris@gmail.com>
2011-03-18 04:00:32 -0400

    Added Climate Activity class

---
+++ src/com/morris/scott/autohome/ClimateActivity.java
@@ -0,0 +1,15 @@
+package com.morris.scott.autohome;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ClimateActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.climate);
+	}
+	
+
+}
