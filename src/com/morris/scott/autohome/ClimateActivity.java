ec4743282e597bd94e0e774fdc4e5b85d3d549f8
Scott Morris <scott.moris@gmail.com>
2011-03-22 01:19:20 -0400

    Added Status Icons and new Title Bar colour

--- src/com/morris/scott/autohome/ClimateActivity.java
+++ src/com/morris/scott/autohome/ClimateActivity.java
@@ -1,7 +1,6 @@
 package com.morris.scott.autohome;
 
 import android.app.Activity;
-import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
